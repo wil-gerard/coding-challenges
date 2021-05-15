a21cb387d7f2522f66c8b2fe94092ee3685b7045
wil-gerard <williamrgerard@gmail.com>
2021-05-14 23:04:36 -0500

    new array challenge and updated methods homework

--- array-methods-hw.js
+++ array-methods-hw.js
@@ -15,18 +15,35 @@
 
 // Give a short description of what the method does, how it works, it's time complexity (if appropriate), and give three examples of it in action!
 
-// .map
-// 
+// ------------.map------------
+// this method calls the function for each element of the array and returns an array of results
 
-// .reduce
+let lengths = ['Bob', 'John', 'Billiam'].map(item => item.length);
 
-// .filter
+alert(lengths); // 3,4,7
 
-// .forEach
+// ------------.reduce------------
+// this method is used to calculate a single value based on the array. it calls a function that is applied to all elements one after the other and accumulates its result to the next call.
 
-// .sort
 
-// .slice
+let arr = [1, 2, 3, 4, 5];
+
+let result = arr.reduce((sum, current) => sum + current, 0);
+
+alert(result); // 15
+
+// given an array of numbers, this will return the average. using .reduce to find the sum of the array 
+function getAverage(marks){
+    return Math.floor(marks.reduce((a, b) => a + b, 0) / marks.length)
+ }
+
+// ------------.filter------------
+
+// ------------.forEach------------
+
+// ------------.sort------------
+
+// ------------.slice------------
 // this extracts a section of a string based on the index supplied and returns a new string. slice takes in the first parameter as the beginning index and an optional ending index as the second parameter. if a negative index is used, it will slice backwards from the end of the string
 
 let str = 'I want to be the very best'
@@ -35,21 +52,21 @@ console.log(str.slice(13)) // 'the very best'
 console.log(str.slice(0, 6)) // 'I want'
 console.log(str.slice(-9)) // 'very best
 
-// .pop
+// ------------.pop------------
 // extracts an item from the end of an array
 
-// .shift
+// ------------.shift------------
 // extracts an items from the beginning of an array
 
-// .push
+// ------------.push------------
 // adds an item from the end of an array
 
-// .unshift
+// ------------.unshift------------
 // adds an item to the beginning of an array
 
-// .includes
+// ------------.includes------------
 
-// .indexOf
+// ------------.indexOf------------
 // this method finds the index of a subtring and returns the position. if no reference is found, it returns -1
 const author = 'Stephen King'
 const searchTerm1 = 'King'
@@ -58,4 +75,4 @@ const searchTerm2 = 'Orwell'
 console.log(author.indexOf(searchTerm1)) // 8
 console.log(author.indexOf(searchTerm2)) // -1
  
-// .every
+// ------------.every------------
--- challenge-25.js
+++ challenge-25.js
@@ -1,1 +1,12 @@
-// test
+// Array Challenge #5
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+// Return the average of the given array rounded down to its nearest integer.
+
+// The array will never be empty.
+
+let arr = [4,5,1]
+
+function getAverage(marks){
+   return Math.floor(marks.reduce((a, b) => a + b, 0) / marks.length)
+}
