4a9db565700d4e72553b969fc60dada76784b790
wil-gerard <williamrgerard@gmail.com>
2021-05-25 17:34:43 -0500

    basic descriptions of all string methods added and at least one example

--- string-methods.js
+++ string-methods.js
@@ -19,22 +19,26 @@
 
 // ------------------------ charAt ------------------------
 // .charAt() method is used to find the character at a given index
+
 let str = "HELLO WORLD";
 str.charAt(0); // returns H
 
 // ------------------------ charCodeAt ------------------------
 // .charCodeAt() returns the unicode (UTF-16) at a specified index
+
 let str = "HELLO WORLD";
 str.charCodeAt(0);         // returns 72
 
 // ------------------------ concat ------------------------ 
 // .concat() joins two or more strings
+
 let text1 = "Hello";
 let text2 = "World";
 let text3 = text1.concat(" ", text2);
 
 // ------------------------ includes ------------------------
 // .includes() asks if the given string paramenter is within the array. returning true or false
+
 const array1 = [1, 2, 3];
 
 console.log(array1.includes(2));
@@ -50,6 +54,7 @@ console.log(pets.includes('at'));
 
 // ------------------------ indexOf ------------------------
 // .indexOf() returns the index of the first instance of a string. if none are found, -1 is returned. a second parameter is accepted as the index to start searching from
+
 let str = "Please locate where 'locate' occurs!";
 let pos = str.indexOf("locate");
 // returns 7
@@ -78,6 +83,7 @@ console.log(`Gotta catchem all ${poke.repeat(3)}`);
 
 // ------------------------ replace ------------------------
 // .replace() takes in two parameters and searches the given string for all instances of the first given parameter, then replaces with the second given parameter, returning a new string. if a string is given for the first parameter, only the first instance is replaced, if a regex is given, all instances are replaced. the second parameter can be a string or a function to be called for each match.
+
 const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
 
 console.log(p.replace('dog', 'monkey'));
@@ -117,16 +123,65 @@ str.slice(-3, -1)  // returns 'us'
 str.slice(0, -1)   // returns 'The morning is upon us'
 
 // ------------------------ split ------------------------ 
+// .split() divides a string into an ordered list of substrings returns them in an array. it divides the string by the provided pattern in the first parameter. the second parameter limits the number of substrings to be included in the array.
 
+const myString = 'Hello World. How are you doing?'
+const splits = myString.split(' ', 3)
+
+console.log(splits)
+// returns ["Hello", "World.", "How"]
+
+const str = 'asdfghjkl'
+const strReverse = str.split('').reverse().join('')
+// 'lkjhgfdsa'
+
+const names = 'Harry Trump ;Fred Barney; Helen Rigby ; Bill Abel ;Chris Hand '
+const re = /\s*(?:;|$)\s*/
+const nameList = names.split(re)
+
+console.log(nameList)
+// returns [ "Harry Trump", "Fred Barney", "Helen Rigby", "Bill Abel", "Chris Hand", "" ]
 
 // ------------------------ substr ------------------------ 
+// .substr() returns a portion of the given string starting at the given index from the first parameter and extending for a given number of characters after from the second parameter. if no second parameter is given, the rest of the string is returned
+
+const str = 'Mozilla';
+
+console.log(str.substr(1, 2));
+// expected output: "oz"
+
+console.log(str.substr(2));
+// expected output: "zilla"
 
 
 // ------------------------ toLowerCase ------------------------ 
+// .toLowerCase() returns the given string in all lower case characters
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toLowerCase());
+// expected output: "the quick brown fox jumps over the lazy dog."
+
 
 
 // ------------------------ toUpperCase ------------------------ 
+// .toUpperCase() returns the given string in all upper case characters
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(sentence.toUpperCase());
+// expected output: "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
 
 
 // ------------------------ trim ------------------------ 
+// .trim() returns the given string with whitespace from both ends removed. this includes all whitespace characters and line terminator characters (LF, CR, etc.)
+
+const greeting = '   Hello world!   ';
+
+console.log(greeting);
+// expected output: "   Hello world!   ";
+
+console.log(greeting.trim());
+// expected output: "Hello world!";
+
 
