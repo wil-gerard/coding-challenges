313321c948011ec2aa9ce38276d9530459f60bbd
wil-gerard <williamrgerard@gmail.com>
2021-07-31 16:19:39 -0500

    new challenge

--- challenge-53.js
+++ challenge-53.js
@@ -1,20 +1,37 @@
-/* In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+/* Create a function that returns the name of the winner in a fight between two fighters.
 
-For example:
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
 
-dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
-dup(["kelless","keenness"]) = ["keles","kenes"].
-Strings will be lowercase only, no spaces. See test cases for more examples.
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
 
-Good luck! */
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
 
-// map through the arrays, each return in the map should be the string split and filtering for each letter that does not equal the one before it then join them up - we had this pattern before!
+Example:
+function Fighter(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function() { return this.name; }
+} */
 
-function dup(s) {
-    let splitArr = s.map(word => word.split(''))
-    return splitArr.filter((value, index, array) => {
-        for (let i = array.length + 1; i > 0;) {
-            array[i] == array[i + 1] ? i-- : array[i] 
-        }
-    })
-};
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    if (firstAttacker == fighter1.name) {
+        let attacker = fighter1
+        let defender = fighter2 - fighter1.damagePerAttack
+        let attackerHealth = fighter1.health
+        let defenderHealth = fighter2.health - fighter1.damagePerAttack
+
+        console.log(`The attacker is ${fighter1} with ${attackerHealth} starting health and the defender is ${fighter2} with ${defenderHealth} health after the first attack`)
+    } else if (firstAttacker == fighter2.name) {
+        let attacker = fighter2
+        let defender = fighter1 - fighter2.damagePerAttack
+        let attackerHealth = fighter2.health
+        let defenderHealth = fighter1.health - fighter2.damagePerAttack
+        console.log(`The attacker is ${fighter2} with ${attackerHealth} starting health and the defender is ${fighter1} with ${defenderHealth} health after the first attack`)
+    }
+
+    for (let i = attackerHealth; i <= 0; i - defender.damagePerAttack){
+        if (i < )
+    }
+}
