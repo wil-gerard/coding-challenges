4825e519486e378a89150609b4aaad7439322a49
wil-gerard <williamrgerard@gmail.com>
2021-08-30 13:12:51 -0500

    new challenges

---
+++ challenge-81.js
@@ -0,0 +1,1 @@
+/* Every year that is exactly divisible by four is a leap year, except for years that are exactly divisible by 100, but these centurial years are leap years if they are exactly divisible by 400. For example, the years 1700, 1800, and 1900 are not leap years, but the years 1600 and 2000 are. */
