9f922955da00fc6603e9f510b280484b23d85e5a
wil-gerard <williamrgerard@gmail.com>
2021-09-13 15:12:34 -0500

    new challenge

---
+++ challenge-96.js
@@ -0,0 +1,12 @@
+// 8 kyu
+/* Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+doubleChar("String") ==> "SSttrriinngg"
+
+doubleChar("Hello World") ==> "HHeelllloo  WWoorrlldd"
+
+doubleChar("1234!_ ") ==> "11223344!!__  " */
+
+function doubleChar(str) {
+     return str.split('').map(letter => letter + letter).join('')
+}
