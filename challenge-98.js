7c2c1bba1048d24a71049dc3ba94d7dcfc5b732e
wil-gerard <williamrgerard@gmail.com>
2021-09-21 18:49:09 -0500

    new challenge

--- challenge-98.js
+++ challenge-98.js
@@ -1,3 +1,4 @@
+// 8 kyu
 /* You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
 
 The returned value must be a string, and have "***" between each of its letters.
