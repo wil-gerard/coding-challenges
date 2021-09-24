c8e68a52dd2839cec4ba782c5b3ee3626a7b6231
wil-gerard <williamrgerard@gmail.com>
2021-09-24 15:45:47 -0500

    start of new challenge

---
+++ challenge-101.js
@@ -0,0 +1,18 @@
+// 6ku
+/* Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid. */
+function letterValue() {
+    
+}
+
+function high(x) {
+
+    x.split(' ').map((stringSubarray) => stringSubarray.replace(/[a-z]/g, ))
+}
