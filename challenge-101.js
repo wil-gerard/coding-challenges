3fc7609e2c40efa8cffec4bd58d983fec78c4d8a
wil-gerard <williamrgerard@gmail.com>
2021-09-27 16:34:14 -0500

    new challenge using object oriented programming | split, reduce, map, indexOf, and  Math.max

--- challenge-101.js
+++ challenge-101.js
@@ -8,11 +8,39 @@ You need to return the highest scoring word as a string.
 If two words score the same, return the word that appears earliest in the original string.
 
 All letters will be lowercase and all inputs will be valid. */
-function letterValue() {
-    
+
+const alphabetScore = {
+    "a" : 1,
+    "b" : 2,
+    "c" : 3,
+    "d" : 4,
+    "e" : 5,
+    "f" : 6,
+    "g" : 7,
+    "h" : 8,
+    "i" : 9,
+    "j" : 10,
+    "k" : 11,
+    "l" : 12,
+    "m" : 13,
+    "n" : 14,
+    "o" : 15,
+    "p" : 16,
+    "q" : 17,
+    "r" : 18,
+    "s" : 19,
+    "t" : 20,
+    "u" : 21,
+    "v" : 22,
+    "w" : 23,
+    "x" : 24,
+    "y" : 25,
+    "z" : 26,
 }
 
-function high(x) {
-
-    x.split(' ').map((stringSubarray) => stringSubarray.replace(/[a-z]/g, ))
+function high(x){
+    const splitStringArray = x.split(' ')
+    const wordScoreArray = splitStringArray.map(word => word.split('').reduce((sum, currentValue) => sum + alphabetScore[currentValue], 0))
+    const indexOfHighscore = wordScoreArray.indexOf(Math.max(...wordScoreArray))
+    return splitStringArray[indexOfHighscore]
 }
