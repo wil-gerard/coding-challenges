66d6609385b7c4459400c430545d30a7c6f3fc3e
wil-gerard <williamrgerard@gmail.com>
2021-09-28 13:43:59 -0500

    new challenge

---
+++ challenge-103.js
@@ -0,0 +1,45 @@
+/* Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid. */
+
+const alphabetScore = {
+    "a" : 1,
+    "b" : 2,
+    "c" : 3,
+    "d" : 4,
+    "e" : 5,
+    "f" : 6,
+    "g" : 7,
+    "h" : 8,
+    "i" : 9,
+    "j" : 10,
+    "k" : 11,
+    "l" : 12,
+    "m" : 13,
+    "n" : 14,
+    "o" : 15,
+    "p" : 16,
+    "q" : 17,
+    "r" : 18,
+    "s" : 19,
+    "t" : 20,
+    "u" : 21,
+    "v" : 22,
+    "w" : 23,
+    "x" : 24,
+    "y" : 25,
+    "z" : 26,
+}
+
+function high(x){
+    const splitString = x.split(' ')
+    const wordScore = splitString.map(word => word.split('').reduce((score, currentLetter) => score + alphabetScore[currentLetter], 0))
+    const highestIndex = wordScore.indexOf(Math.max(...wordScore))
+    return splitString[highestIndex]
+}
