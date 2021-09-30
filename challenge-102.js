662bda2801d9ae518d6b74a2f2776a7e3a62877f
wil-gerard <williamrgerard@gmail.com>
2021-09-30 17:42:46 -0500

    new challenge:

--- challenge-102.js
+++ challenge-102.js
@@ -7,3 +7,26 @@ Examples
 "(( @"     =>  "))(("  */
 
 // https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+
+function duplicateEncode(word){
+    return word.toLowerCase().split('').map((letter, index) => word.indexOf(letter) === 1 ? letter = '(' : letter = ')' ).join('')
+
+}
+
+function duplicateEncode(word){
+    return word.toLowerCase().split('').map((letter, index) => console.log(`${index}` + word.indexOf(letter, index)))
+}
+  
+function duplicateEncode(word){
+    console.log( word.toLowerCase().split(''))
+}
+
+function duplicateEncode(string){
+    return string
+                .toLowerCase()
+                .split('')
+                .map((letter, index, array) => array.indexOf(letter) === array.lastIndexOf(letter) ?
+                    letter = '(' :
+                    letter = ')')
+                .join('')   
+}
