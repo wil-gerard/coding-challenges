41d0e0b9fcb9126e2bde1f02225db70a54e7b5d5
wil-gerard <williamrgerard@gmail.com>
2021-10-05 15:01:27 -0500

    new challenge

---
+++ challenge-106.js
@@ -0,0 +1,5 @@
+/* Implement a function which converts the given boolean value into its string representation. */
+
+function booleanToString(b){
+    return b === true ? b = 'true' : b = 'false'
+}
