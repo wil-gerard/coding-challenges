4fce8333fd11492b99872f2850b2246aab781571
wil-gerard <williamrgerard@gmail.com>
2021-10-11 18:29:29 -0500

    new challenge

--- challenge-109.js
+++ challenge-109.js
@@ -1,13 +1,26 @@
-/* create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+// create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 
-Ex.
-'eloquent' => 'loquen'
-'country' => 'ountr' */
+// Ex.
+// 'eloquent' => 'loquen'
+// 'country' => 'ountr'
 
-const array1 = [1, 4, 9, 16];
+// Parameter: A string with more than two characters
+// Returns: The string with the first and last characters removed
 
-// pass a function to map
-const map1 = array1.map((x, index, array) => x * 2);
+// function firstAndLastRemoved(x) {
+//   let newStr = ''
+//   for(let i = 1; i <= x.length - 2; i++) {
+//     newStr += x[i]
+//   }
 
-console.log(map1);
-// expected output: Array [2, 8, 18, 32]
+//   return newStr
+// }
+
+function firstAndLastRemoved(x) {
+  return x.slice(1, -1)
+}
+
+
+console.log(firstAndLastRemoved('booop'), 'ooo')
+console.log(firstAndLastRemoved('down'), 'ow')
+console.log(firstAndLastRemoved('hhiiiiiii'), 'hiiiiii')
